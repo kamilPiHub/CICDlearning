8939975f9ede475cb3ba96e738692a5a42782f49
kamilPiHub <pizon792@gmail.com>
2026-04-10 13:47:55 +0200

    add port in backend/app.py

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask,jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
-    return "CICD Pipeline is working!"
+    return jsonify({
+        "status": "success", 
+        "message": "CICD Pipeline is working!"
+        })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
